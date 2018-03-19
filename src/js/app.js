5768686265294330367f21634d2f64ff4125fe1e
Chrystelle Poulet <chrystelle.poulet@hetic.net>
2018-03-19 22:08:00 +0100

    cleaning up our response

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,10 @@
 //require the request package
 const request = require('request');
 
+let apiKey = 'd8230ded1f1b2b62ceb2af8d3046693d';
+let city = 'paris';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}` // Open Weather App defaults its temperature to Kelvin. units=metric was added to use Celsius
+
 //pass in a url and request returns a callback function with 3 arguments
 request(url, function (err, response, body) {
   //check for an error
@@ -8,6 +12,8 @@ request(url, function (err, response, body) {
     console.log('error:', error);
   // if no error, log the entire contents of the response body
   } else {
-    console.log('body:', body);
+    let weather = JSON.parse(body);
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`
+    console.log(message);
   }
 })
